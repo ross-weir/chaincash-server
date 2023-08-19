1c118e1fdb2c5a28c92bd854e2cc0ccb0fa46d1e
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-08-19 21:23:11 +1000

    add log level cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! ChainCash server CLI.
 use clap::Parser;
 use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 /// ChainCash payment server
 #[derive(Parser, Debug)]
@@ -9,13 +10,26 @@ struct Args {
     /// The address to listen on
     #[clap(short, long, default_value = "127.0.0.1:8080")]
     listen: String,
+
+    /// The level of logging to use for the server
+    #[clap(long, default_value = tracing::Level::INFO.as_str())]
+    log_level: tracing::Level,
 }
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
-
     let args = Args::parse();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                format!("chaincash_server={},axum::rejection=trace", args.log_level).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     info!("started with {:?}", args);
 
